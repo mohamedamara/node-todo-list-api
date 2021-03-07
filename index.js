ab25ecbca78a352a260cce5255779127cdec31bc
Mohamed Amara <20605958+mohamedamara@users.noreply.github.com>
2021-03-07 21:41:01 +0100

    intial server and packages setup

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
-console.log("Hello World");
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/", (req, res) => {
+  res.json({ msg: "Hello from Todo list app" });
+});
+
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
