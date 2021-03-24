f0e3455027089a4676ed0a30962d5bc9784fa9f0
Mohamed Amara <20605958+mohamedamara@users.noreply.github.com>
2021-03-24 22:37:21 +0100

    fix: allow CORS

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
+const cors = require("cors");
 const databaseConnection = require("./config/database_connection");
 const usersRoute = require("./routes/users_route");
 const authRoute = require("./routes/auth_route");
 const todosRoute = require("./routes/todos_route");
 
 const app = express();
+app.use(cors());
 app.use(express.json({ extended: false }));
 const port = process.env.PORT || 5000;
 
