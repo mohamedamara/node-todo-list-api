18094d4e55f03dc0420a7dd0c8f849657b54af48
Mohamed Amara <20605958+mohamedamara@users.noreply.github.com>
2021-04-13 21:36:30 +0100

    refactor: todo  title must be provided

--- middlewares/validators/todos_route_validation.js
+++ middlewares/validators/todos_route_validation.js
@@ -2,7 +2,7 @@ const { body, validationResult } = require("express-validator");
 
 exports.addNewTodoValidation = [
   body("todoTitle")
-    .optional()
+    .notEmpty()
     .isString()
     .withMessage("Todo title most be a String"),
   body("todoContent")
@@ -20,17 +20,13 @@ exports.addNewTodoValidation = [
 
 exports.updateTodoValidation = [
   body("todoTitle")
-    .optional()
+    .notEmpty()
     .isString()
     .withMessage("Todo title most be a String"),
   body("todoContent")
     .optional()
     .isString()
     .withMessage("Todo content most be a String"),
-  body("completed")
-    .optional()
-    .isBoolean()
-    .withMessage("Completed most be a Boolean"),
   (req, res, next) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
